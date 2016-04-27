badddd8e56e8d4cd2324f3394ad5ce98a729b565
bonetaurelie <bonetaurelie@yahoo.fr>
2016-04-27 15:44:43 +0200

    création du formulaire visiteur

---
+++ src/AB/CoreBundle/Form/VisiteurType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AB\CoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class VisiteurType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('dateNaissance', 'date')
+            ->add('pays')
+            ->add('tarifReduit')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AB\CoreBundle\Entity\Visiteur'
+        ));
+    }
+}
