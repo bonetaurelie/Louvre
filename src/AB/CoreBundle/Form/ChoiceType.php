1a9cdf4afc06333e5877f6997126965d2b23405b
bonetaurelie <bonetaurelie@yahoo.fr>
2016-04-27 14:49:22 +0200

    création du formulaire ChoiceType pour faire menu déroulant dans BilletType

--- src/AB/CoreBundle/Form/ChoiceType.php
+++ src/AB/CoreBundle/Form/ChoiceType.php
@@ -6,7 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class TypeType extends AbstractType
+class ChoiceType extends AbstractType
 {
     /**
      * @param FormBuilderInterface $builder
