0fc0d75221a1d374307cb149d166045505327fa7
bonetaurelie <bonetaurelie@yahoo.fr>
2016-04-27 14:05:08 +0200

    création du formulaire type de billet

---
+++ src/AB/CoreBundle/Form/TypeType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AB\CoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TypeType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('demiJournee')
+            ->add('journee')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AB\CoreBundle\Entity\Type'
+        ));
+    }
+}
