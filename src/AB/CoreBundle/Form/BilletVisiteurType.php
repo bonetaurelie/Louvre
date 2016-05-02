fe3a951967d028902fb5785dc015252f204384be
bonetaurelie <bonetaurelie@yahoo.fr>
2016-05-02 22:16:55 +0200

    création formulaire faire boucle formulaire visiteur

---
+++ src/AB/CoreBundle/Form/BilletVisiteurType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AB\CoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+
+class BilletVisiteurType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add("visiteurs", CollectionType::class, array(
+                // each entry in the array will be an "email" field
+                'entry_type'   => VisiteurType::class,
+            ))
+            ->add('valider','submit')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AB\CoreBundle\Entity\Billet'
+        ));
+    }
+}
