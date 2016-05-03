926f906b890abf987143d8894b4e4747fc73e682
bonetaurelie <bonetaurelie@yahoo.fr>
2016-05-03 12:16:41 +0200

    création vue paiement

---
+++ src/AB/CoreBundle/Entity/Validation_commande.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace AB\CoreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Validation_commande
+ *
+ * @ORM\Table(name="validation_commande")
+ * @ORM\Entity(repositoryClass="AB\CoreBundle\Repository\Validation_commandeRepository")
+ */
+class Validation_commande
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="montant", type="integer")
+     */
+    private $montant;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="statut", type="string", length=255)
+     */
+    private $statut;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set montant
+     *
+     * @param integer $montant
+     * @return Validation_commande
+     */
+    public function setMontant($montant)
+    {
+        $this->montant = $montant;
+
+        return $this;
+    }
+
+    /**
+     * Get montant
+     *
+     * @return integer 
+     */
+    public function getMontant()
+    {
+        return $this->montant;
+    }
+
+    /**
+     * Set statut
+     *
+     * @param string $statut
+     * @return Validation_commande
+     */
+    public function setStatut($statut)
+    {
+        $this->statut = $statut;
+
+        return $this;
+    }
+
+    /**
+     * Get statut
+     *
+     * @return string 
+     */
+    public function getStatut()
+    {
+        return $this->statut;
+    }
+}
