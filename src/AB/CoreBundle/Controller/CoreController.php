8ebbe421853ac5484d322b374a560e43c0e9ddda
bonetaurelie <bonetaurelie@yahoo.fr>
2016-05-04 08:36:20 +0200

    contrainte billet journée jour j 14h

--- src/AB/CoreBundle/Controller/CoreController.php
+++ src/AB/CoreBundle/Controller/CoreController.php
@@ -3,6 +3,7 @@
 namespace AB\CoreBundle\Controller;
 
 use AB\CoreBundle\Entity\Billet;
+use AB\CoreBundle\Entity\Commande;
 use AB\CoreBundle\Entity\Visiteur;
 use AB\CoreBundle\Form\BilletType;
 use AB\CoreBundle\Form\BilletVisiteurType;
@@ -18,9 +19,11 @@ class CoreController extends Controller
     }
 
     public function reservationAction( Request $request){
+        $error="";
+        $error1="";
         $billet= new Billet();
         $billet->setDate( new \Datetime());
-        $resa=$billet->setDateResa(new \DateTime());
+        $billet->setDateResa(new \DateTime());
         $now = new \DateTime(date("d/m/Y 14:00:00"));
        
         $form= $this->get('form.factory')->create(new BilletType(),$billet);
@@ -28,22 +31,21 @@ class CoreController extends Controller
             $em=$this->getDoctrine()->getManager();
             $billets = $this->getDoctrine()->getRepository("ABCoreBundle:Billet")->findByDate($billet->getDate());
             if(count($billets) > 1000){
-                $request->getSession()->getFlashBag()->add('notice','Impossible de réserver pour ce jour, nous sommes complet, désolé !');
-            }elseif (($resa==$now && $billet->getDateResa()>=$now) && ($billet->getType()=='journee')){
-              $request->getSession()->getFlashBag()->add('notice','Impossible de réserver pour ce jour, nous sommes complet, désolé !');
+                $error="Impossible de réserver pour ce jour, nous sommes complet, désolé !";
+            }elseif (($billet->getDateResa()>=$now) && ($billet->getType()==='journee')){
+                $error1="Impossible de réserver un billet journée une fois 14h passées";
             }else {
                 $em->persist($billet);
                 $em->flush();
                 return $this->redirect($this->generateUrl('ab_core_visiteur',array('id'=>$billet->getId())));
             }
         }
-        return $this->render('ABCoreBundle:Default:reservation.html.twig',array('billet'=>$billet,'form'=>$form->createView()));
+        return $this->render('ABCoreBundle:Default:reservation.html.twig',array('billet'=>$billet,'form'=>$form->createView(),'error'=>$error,'error1'=>$error1));
     }
 
     public function visiteurAction($id, Request $request){
         $billet = $this->getDoctrine()->getRepository("ABCoreBundle:Billet")->find($id);
 
-//
         $form= $this->get('form.factory')->create(new BilletVisiteurType(),$billet);
 
         for($a = 0;$a < $billet->getQuantite();$a++){
@@ -60,6 +62,47 @@ class CoreController extends Controller
             }
             $em->persist($billet);
             $em->flush();
+            $billet=$this->getDoctrine()->getRepository('ABCoreBundle:Billet')->findByDate($billet->getDate());
+            $visiteur=$this->getDoctrine()->getRepository('ABCoreBundle:Visiteur')->findByBillet($visiteur->getBillet());
+            $commande = new Commande();
+            $em=$this->getDoctrine()->getManager();
+            $commande->setDateResa($billet);
+            $commande->setNom($visiteur);
+            $now=new\ Datetime('today');
+            //personne de plus de 12ans 16€
+            if($now>$birthday= new \Datetime('today -12 years')){
+                $commande->setTarif(16);
+            }
+            //personne entre 4 et 12 ans 8€
+            elseif ($now>$birthday= new \Datetime('today between -4 years and -12 years')){
+                $commande->setTarif(8);
+            }
+            //-4 ans gratuit
+            elseif ($now>$birthday= new \Datetime('today between today and -4years')){
+                $commande->setTarif(0);
+            }
+            //+60 ans 12€
+            elseif ($now>$birthday= new \Datetime('today -60years')){
+                $commande->setTarif(12);
+            }
+            //tarif réduit coché 10€
+            elseif ($visiteur->getTarifReduit()===1){
+                $commande->setTarif(10);
+            }
+            //il faut 4 fois le même nom de famille +2 adultes et 2 enfants
+            elseif ($billet->getNom()){
+                $commande->setTarif('35');
+            }
+
+            //enregistrement du code resa
+            $code=$visiteur->getNom().$visiteur->getDateResa();
+            $visiteur->setCodeResa($code);
+
+            //enregistrement du crquode
+
+            $em->persist($commande);
+            $em->flush();
+
 
             return $this->redirect($this->generateUrl('ab_core_paiement'));
         }
