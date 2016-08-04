40cb838c87ba3560af5df9fe1a0a3ef40c5500f8
bonetaurelie <bonetaurelie@yahoo.fr>
2016-08-04 14:43:58 +0200

    màj paiement paypal

--- src/AB/CoreBundle/Controller/OrderController.php
+++ src/AB/CoreBundle/Controller/OrderController.php
@@ -200,81 +200,84 @@ class OrderController extends Controller
         return $this->render('ABCoreBundle:Default:paiement.html.twig', array('id' => $val_commande->getId()));
     }
 
-    public function completeAction($id){
+    public function partageAction($id, Request $request){
         $em = $this->getDoctrine()->getManager();
         $val_commande = $em->getRepository('ABCoreBundle:Validation_commande')->find($id);
 
-        if (!$this->getRequest()->get('token') || !$this->getRequest()->get('PayerID'))
-            throw $this->createNotFoundException();
-        if (null != $val_commande->getPaypalDetails() || null != $val_commande->getPaypalComplete())
-            throw $this->createNotFoundException();
+        if ($request->getMethod('POST')) {
+            $user = 'bonetaurelie-facilitator_api1.gmail.com';
+            $pwd = '3VTNTX4M4PDAXA9P';
+            $signature = 'AFydXqgoC9ryJcgfJfQdpyqb9ioWAZYr6xGoUo-Jtcv0YluatYz.z17B';
 
-        $em = $this->getDoctrine()->getEntityManager();
+            $params = array(
+                'TOKEN' => urldecode($request->get('token')),
+                'METHOD' => 'GetExpressCheckoutDetails',
+                'VERSION' => '204.0',
+                'USER' => $user,
+                'PWD' => $pwd,
+                'SIGNATURE' => $signature
+            );
 
-        $params = array(
-            'METHOD'    => 'GetExpressCheckoutDetails',
-            'TOKEN'     => urldecode($this->getRequest()->get('token')),
-        );
+            $params = http_build_query($params);
+            $endpoint = 'https://api-3t.sandbox.paypal.com/nvp';
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => $endpoint,
+                CURLOPT_POST => 1,
+                CURLOPT_POSTFIELDS => $params,
+                CURLOPT_RETURNTRANSFER => 1,
+                CURLOPT_SSL_VERIFYPEER => false,
+                CURLOPT_SSL_VERIFYHOST => false,
+                CURLOPT_VERBOSE => 1
+            ));
 
-        $orderDetails = $this->getPaypalParam($this->sendPaypal($params));
+            $response = curl_exec($curl);
+            $responseArray = array();
+            parse_str($response, $responseArray);
+            if (curl_errno($curl)) {
+                $this->get('session')->getFlashBag()->add('error', $this->get('translator')->trans('echec.message'));
+                return $this->redirect($this->generateUrl('ab_core_paiement', array('id' => $val_commande->getId())));
+                curl_close($curl);
+            } else {
+                if ($responseArray['ACK'] == 'Success') {
+                    return $this->redirect('https://www.sandbox.paypal.com/webscr?cmd=_express-checkout&useraction=commit&token=' . $responseArray['TOKEN']);
+                } else {
+                    $this->get('session')->getFlashBag()->add('error', $this->get('translator')->trans('echec.message'));
+                    return $this->redirect($this->generateUrl('ab_core_paiement', array('id' => $val_commande->getId())));
 
-        $val_commande->setTarif($orderDetails['TARIF']);
-        $em->flush();
+                }
+                curl_close($curl);
+            }
+            curl_close($curl);
+        }
 
-        $params = array(
-            'PAYMENTREQUEST_0_PAYMENTACTION' => 'Sale',
-            'PAYERID' => urldecode($this->getRequest()->get('PayerID')),
-            'TOKEN' => urldecode($this->getRequest()->get('token')),
-            'PAYMENTREQUEST_0_AMT' => $val_commande->getTarif(),
-            'PAYMENTREQUEST_0_CURRENCYCODE' => 'EUR',
-            'METHOD' => 'DoExpressCheckoutPayment'
-        );
 
-        $val_commande->setStatut('paypal');
-        $em->flush();
-
-        return $this->redirect($this->get('router')->generate('ab_core_partage'));
-    }
-
-    private function sendPaypal($params)
-    {
-        $api_paypal = $this->container->getParameter('https://api-3t.sandbox.paypal.com/nvp');
-        $version = $this->container->getParameter('204.0');
-        $user = $this->container->getParameter('bonetaurelie-facilitator_api1.gmail.com');
-        $pass =  $this->container->getParameter('3VTNTX4M4PDAXA9P');
-        $signature =  $this->container->getParameter('AFydXqgoC9ryJcgfJfQdpyqb9ioWAZYr6xGoUo-Jtcv0YluatYz.z17B');
-        //construction de l'url
-        $url = $api_paypal.'?VERSION='.$version.
-            '&USER='.$user.
-            '&PWD='.$pass.
-            '&SIGNATURE='.$signature;
-        foreach($params as $k => $v)
-            $url .= '&'.$k."=".urlencode($v);
-        $ch =curl_init($url);
-
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $res =  curl_exec($ch);
-        curl_close($ch);
-        if (!$res)
-        {
+        if (!$request->get('token')) {
             $this->get('session')->getFlashBag()->add('error', $this->get('translator')->trans('echec.message'));
             return $this->redirect($this->generateUrl('ab_core_paiement', array('id' => $val_commande->getId())));
         }
-             return $res;
-    }
 
-    private function getPaypalParam($url)
-    {
-        $params = array();
 
-        $lst_params = explode('&', $url);
-        foreach($lst_params as $param_paypal)
-        {
-            list($nom, $valeur) = explode("=", $param_paypal);
-            $params[$nom]=urldecode($valeur);
+        $params = array(
+            'METHOD'    => 'DoExpressCheckouptPayment',
+            'TOKEN'     => urldecode($request->get('token')),
+            'PAYERID'     => urldecode($request->get('PayerID')),
+            'PAYMENTACTION'=>'Sale',
+            'PAYMENTREQUEST_0_AMT'=>$val_commande->getTarif(),
+            'PAYMENTCURRENCYCODE'=>'EUR'
+        );
+
+        if($params){
+            $val_commande->setStatut('paypal');
+            $em->flush();
+        }
+        else{
+            $this->get('session')->getFlashBag()->add('error', $this->get('translator')->trans('echec.message'));
+            return $this->redirect($this->generateUrl('ab_core_paiement', array('id' => $val_commande->getId())));
         }
 
-        return $params;
+
+
+        return $this->redirect($this->get('router')->generate('ab_core_partage'));
     }
 }
