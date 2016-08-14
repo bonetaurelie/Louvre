9910e6396965ec8fefa1a0b8081e9b19e5a36d2b
Christophe Lablancherie <c.lablancherie@live.fr>
2016-08-14 13:19:18 +0200

    Ajout service

--- src/AB/CoreBundle/Controller/CoreController.php
+++ src/AB/CoreBundle/Controller/CoreController.php
@@ -2,15 +2,8 @@
 
 namespace AB\CoreBundle\Controller;
 
-use AB\CoreBundle\Entity\Billet;
-use AB\CoreBundle\Entity\Commande;
-use AB\CoreBundle\Entity\Validation_commande;
-use AB\CoreBundle\Entity\Visiteur;
-use AB\CoreBundle\Form\BilletType;
-use AB\CoreBundle\Form\BilletVisiteurType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use AB\CoreBundle\Form\VisiteurType;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -20,7 +13,6 @@ class CoreController extends Controller
 
     public function indexAction(Request $request)
     {
-        $locale= $request->getLocale();
         return $this->render('ABCoreBundle:Default:index.html.twig');
     }
    
@@ -28,12 +20,15 @@ class CoreController extends Controller
         $commande= $this->getDoctrine()->getManager()->getRepository('ABCoreBundle:Commande')->find($id);
         return $this->render('ABCoreBundle:Default:error.html.twig',array('commande'=>$commande));
     }
+
     public function partageAction(){
        
         return $this->render('ABCoreBundle:Default:partage.html.twig');
     }
-    public function onKernelRequest(GestResponseEvent $event){
+
+    //Cette fonction ne sert à rien
+/*    public function onKernelRequest(GestResponseEvent $event){
         $request=$event->getRequest();
         $request->getSession()->set('_locale', $locale);
-    }
+    }*/
 }
